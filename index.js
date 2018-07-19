300f981acac99556bad6320bef723fc3d9c8a1c4
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-19 22:38:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ function destructivelyRemoveFirstKitten(){
 }
 
 function appendKitten(value){
-var newKittens = kittens.splice(kittens.length-1,0,value)
+var newKittens = kittens.splice(1,0,value)
   return kittens
 }
