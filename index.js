fbe145a9f1329cf23dbc91c2eb62a82f2ea1b29f
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-19 22:14:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Add your functions and code here
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
 function destructivelyPrependKitten(givenArray, value){
   givenArray.shift(value)
   return givenArray
