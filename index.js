ed88051dc0017056854b09bcd388f34ba1bdd1b3
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-19 22:16:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your functions and code here
 
-var kittens = ["Milo", "Otis", "Garfield"]
+//var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyPrependKitten(value){
   kittens.push(value)
